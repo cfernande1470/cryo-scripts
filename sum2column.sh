9a7ce237b1b4498cac84980372ca477c97483ee5
Carlos FR <cfernande1470@users.noreply.github.com>
2016-10-31 12:52:48 +0100

    Update sum2column.sh

--- sum2column.sh
+++ sum2column.sh
@@ -5,4 +5,4 @@
 awk '/_rln/ {print}' $1 | nl
 read -p "Column number?" col
 read -p "Add value:" value
-awk '!/@/ {print}' $1 >> $2 && awk -v col=$col -v value=$value '/@/{$col = $col + $value; print}' $1 | column -t >> $2 
+awk '!/@/ {print}' $1 > $2 && awk -v col=$col -v value=$value '/@/{$col = $col + $value; print}' $1 | column -t >> $2 
